92f1b28d2a0cd9acd3981c16361438467ea95c90
BEN ABDESSALEM Leith <lbenabdessalem@g-a102a-07.local>
2020-09-22 17:36:05 +0200

    end of day 03

--- sketch.js
+++ sketch.js
@@ -1,81 +1,111 @@
 let time = 0;
-let speed = 0.04;
+let speed = 0.08;
 
 let w, h;
 
 let cols, rows;
 
-let scale = 20;
+let scale = 32;
 let roughness = 0.05;
-let amount = 550;
+let amount = 750;
 
 let mouseMoveX = 0;
 let mouseMoveY = 0;
 
+let amplitude;
+let smoothVol = 0;
 
 function setup() {
-  createCanvas(windowWidth - 10, windowHeight - 10, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  ortho(-width / 2, width / 2, -height / 2, height / 2, 0, 1400);
   fill(0);
-  w = width;
+  w = width - 100;
   h = height;
   cols = w / scale;
   rows = h / scale;
-  noCursor();
-  
+  cursor(CROSS);
+
   polySynth = new p5.PolySynth();
   userStartAudio();
+
+  amplitude = new p5.Amplitude();
 }
 
 function draw() {
   background(0);
-  time += speed;
+  smoothVol = lerp(smoothVol, amplitude.volNorm, 0.05);
+  time += speed * smoothVol;
 
-  controlCamera();
+  setupCamera();
   drawTerrain();
+  drawArtefact();
   playSoundOnInput();
 }
 
-function controlCamera() {
+function setupCamera() {
   translate(0, 250);
   rotateX(PI / 3);
-  mouseMoveX = lerp(mouseMoveX, (width / 2 - mouseX) * 0.2, 0.2);
-  mouseMoveY = lerp(mouseMoveY, (height / 2 - mouseY) * 0.1, 0.1);
-  translate(-w / 2 + mouseMoveX, -h / 2, -mouseMoveY);
+
+
+  //mouseMoveX = lerp(mouseMoveX, (width / 2 - mouseX) * 0.2, 0.2);
+  //mouseMoveY = lerp(mouseMoveY, (height / 2 - mouseY) * 0.1, 0.1);
+
+
+  translate(-w / 2, -h / 2, 0);
 }
 
 function drawTerrain() {
-  for (var y = 0; y < rows - 1; y++) {
+  for (var y = 0; y <= rows; y++) {
     beginShape(TRIANGLE_STRIP);
-    for (var x = 0; x < cols; x++) {
+    for (var x = 0; x <= cols + 1; x++) {
       let n = noise(x * roughness, y * roughness - time);
       let xPos = x * scale;
       let yPos = y * scale;
 
-      let col = pow(map(y, 0, rows - 1, 1, 6) * map(n, 0, 1, 0.5, 1), 3);
+      let col = pow(map(y, 0, rows - 1, 3, 7) * map(n, 0, 1, 0.5, 1), 3);
       stroke(col);
       strokeWeight(1 + 2 * y / rows);
-      vertex(xPos, yPos, n * amount);
-      vertex(xPos, (y + 1) * scale, noise(x * roughness, (y + 1) * roughness - time) * amount);
+      vertex(xPos, yPos, n * amount * smoothVol);
+      vertex(xPos, (y + 1) * scale, noise(x * roughness, (y + 1) * roughness - time) * amount * smoothVol);
 
     }
     endShape();
   }
 }
 
-function playSoundOnInput()
-{
-  if (keyIsPressed === true) {
-     let dur = 1.5;
+function drawArtefact() {
+  translate(width / 2, height / 2, 500 + sin(time) * 20);
+  rotateX(time);
+  rotateY(time);
+  rotateZ(time);
+  stroke(smoothVol * 200);
+  sphere(100, floor(8 * smoothVol, 0), floor(8 * smoothVol, 0));
+}
 
-  // time from now (in seconds)
+function playSoundOnInput() {
+  let dur = deltaTime / 60;
   let time = 0;
+  let vel = 0.5;
+  if (keyIsPressed === true) {
+    let note = '';
+    switch (keyCode) {
+      case 49: note = 'A3'; break;
+      case 50: note = 'B3'; break;
+      case 51: note = 'C3'; break;
+      case 52: note = 'D3'; break;
+      case 53: note = 'E3'; break;
+      case 54: note = 'F3'; break;
+      case 55: note = 'G3'; break;
+      case 56: note = 'A4'; break;
+      case 57: note = 'B4'; break;
+      case 48: note = 'C4'; break;
+      default: break;
+    }
 
-  // velocity (volume, from 0 to 1)
-  let vel = 0.1;
+    polySynth.play(note, vel, 0, dur)
 
-  // notes can overlap with each other
-  polySynth.play('G2', vel, 0, dur);
-  polySynth.play('C3', vel, time += 1/3, dur);
-  polySynth.play('G3', vel, time += 1/3, dur);
+
+    // polySynth.play('C3', vel, time += 1 / 3, dur);
+    // polySynth.play('G3', vel, time += 1 / 3, dur);
   }
 }
