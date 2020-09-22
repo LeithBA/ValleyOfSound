c86bea67037e60b749cf224cb980a5784bc57a23
Leith B.A <leithba@rog.lan>
2020-09-22 21:22:53 +0200

    Day 02 night edits

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 let time = 0;
-let speed = 0.08;
+let speed = 0.05;
 
 let w, h;
 
 let cols, rows;
 
-let scale = 32;
+let scale = 64;
 let roughness = 0.05;
-let amount = 750;
+let amount = 650;
 
 let mouseMoveX = 0;
 let mouseMoveY = 0;
@@ -19,7 +19,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   ortho(-width / 2, width / 2, -height / 2, height / 2, 0, 1400);
   fill(0);
-  w = width - 100;
+  w = width;
   h = height;
   cols = w / scale;
   rows = h / scale;
@@ -33,7 +33,7 @@ function setup() {
 
 function draw() {
   background(0);
-  smoothVol = lerp(smoothVol, amplitude.volNorm, 0.05);
+  smoothVol = lerp(smoothVol, amplitude.volNorm, 0.05) + 0.002;
   time += speed * smoothVol;
 
   setupCamera();
@@ -55,6 +55,7 @@ function setupCamera() {
 }
 
 function drawTerrain() {
+  fill(0);
   for (var y = 0; y <= rows; y++) {
     beginShape(TRIANGLE_STRIP);
     for (var x = 0; x <= cols + 1; x++) {
@@ -65,7 +66,7 @@ function drawTerrain() {
       let col = pow(map(y, 0, rows - 1, 3, 7) * map(n, 0, 1, 0.5, 1), 3);
       stroke(col);
       strokeWeight(1 + 2 * y / rows);
-      vertex(xPos, yPos, n * amount * smoothVol);
+      vertex(xPos, yPos, n * amount * smoothVol + 0.15);
       vertex(xPos, (y + 1) * scale, noise(x * roughness, (y + 1) * roughness - time) * amount * smoothVol);
 
     }
@@ -78,34 +79,37 @@ function drawArtefact() {
   rotateX(time);
   rotateY(time);
   rotateZ(time);
-  stroke(smoothVol * 200);
-  sphere(100, floor(8 * smoothVol, 0), floor(8 * smoothVol, 0));
+  stroke(smoothVol * 255);
+  sphere(100 * smoothVol, floor(8 * smoothVol, 0), floor(8 * smoothVol, 0));
 }
 
 function playSoundOnInput() {
   let dur = deltaTime / 60;
-  let time = 0;
-  let vel = 0.5;
+  let vel = 1.0;
   if (keyIsPressed === true) {
     let note = '';
-    switch (keyCode) {
-      case 49: note = 'A3'; break;
-      case 50: note = 'B3'; break;
-      case 51: note = 'C3'; break;
-      case 52: note = 'D3'; break;
-      case 53: note = 'E3'; break;
-      case 54: note = 'F3'; break;
-      case 55: note = 'G3'; break;
-      case 56: note = 'A4'; break;
-      case 57: note = 'B4'; break;
-      case 48: note = 'C4'; break;
-      default: break;
-    }
 
-    polySynth.play(note, vel, 0, dur)
+    if (keyCode == 49)
+      polySynth.play("A3", vel, 0, dur);
+    if (keyCode == 50)
+      polySynth.play("B3", vel, 0, dur);
+    if (keyCode == 51)
+      polySynth.play("C3", vel, 0, dur);
+    if (keyCode == 52)
+      polySynth.play("D3", vel, 0, dur);
+    if (keyCode == 53)
+      polySynth.play("E3", vel, 0, dur);
+    if (keyCode == 54)
+      polySynth.play("F3", vel, 0, dur);
+    if (keyCode == 55)
+      polySynth.play("G3", vel, 0, dur);
+    if (keyCode == 56)
+      polySynth.play("A4", vel, 0, dur);
+    if (keyCode == 57)
+      polySynth.play("B4", vel, 0, dur);
+    if (keyCode == 48)
+      polySynth.play("C4", vel, 0, dur);
 
 
-    // polySynth.play('C3', vel, time += 1 / 3, dur);
-    // polySynth.play('G3', vel, time += 1 / 3, dur);
   }
 }
